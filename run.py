9ae9ef1961fd325b184d27c6303b0a516a2b1c81
Aditio <faditiokur@gmail.com>
2021-10-12 05:11:53 +0700

    Adding telegram init

--- run.py
+++ run.py
@@ -2,107 +2,102 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+from telethon import TelegramClient
+from shutil import copytree
+import asyncio
 import time
 import csv
-import subprocess
 
-driver = webdriver.Firefox()
-driver.get("https://web.telegram.org/k/")
 
-time.sleep(30)
-
-waitingDriver = WebDriverWait(driver, 60).until(
-    EC.invisibility_of_element_located((By.CLASS_NAME, 'SearchInput')))
-
-if (waitingDriver == True):
-    btnMenu = driver.find_element_by_css_selector(
-        "div.btn-icon.btn-menu-toggle.rp.sidebar-tools-button")
-    btnMenu.click()
-
-    btnSetting = driver.find_element_by_css_selector(
-        "div.btn-menu-item.tgico-settings.rp")
-    btnSetting.click()
-
-    phoneNumberSpan = driver.find_element_by_css_selector(
-        "div.sidebar-content div.profile-subtitle")
-    phoneNumber = phoneNumberSpan.text.replace(' ', '')
-
+async def telegramInit(phoneNumber, appId, appHash, sessionFolder):
     print(phoneNumber)
 
-    driver.execute_script("window.open('https://my.telegram.org','_blank');")
+    client = TelegramClient(sessionFolder, appId, appHash)
+    
+    await client.start(phoneNumber)
+    await client.sign_in(phoneNumber)
+    code = input('enter code: ')
+    await client.sign_in(phoneNumber, code)
 
-    driver.switch_to.window(driver.window_handles[1])
+    client.disconnect()
 
-    time.sleep(5)
 
-    myPhoneNumber = driver.find_element_by_id("my_login_phone")
-    myBtnSubmit = driver.find_element_by_xpath("//button[@type='submit']")
+def drivingBrowser():
+    driver = webdriver.Firefox()
+    driver.get("https://web.telegram.org/k/")
 
-    myPhoneNumber.send_keys(phoneNumber)
-    myBtnSubmit.click()
+    time.sleep(30)
 
-    driver.switch_to.window(driver.window_handles[0])
+    waitingDriver = WebDriverWait(driver, 60).until(
+        EC.invisibility_of_element_located((By.CLASS_NAME, 'SearchInput')))
 
-    backBtn = driver.find_element_by_css_selector(
-        "button.btn-icon.tgico-left.sidebar-close-button")
-    backBtn.click()
+    if (waitingDriver == True):
+        btnMenu = driver.find_element_by_css_selector(
+            "div.btn-icon.btn-menu-toggle.rp.sidebar-tools-button")
+        btnMenu.click()
 
-    telegramMsg = driver.find_element_by_xpath("//li[@data-peer-id='777000']")
-    telegramMsg.click()
+        btnSetting = driver.find_element_by_css_selector(
+            "div.btn-menu-item.tgico-settings.rp")
+        btnSetting.click()
 
-    time.sleep(5)
+        phoneNumberSpan = driver.find_element_by_css_selector(
+            "div.sidebar-content div.profile-subtitle")
+        phoneNumber = phoneNumberSpan.text.replace(' ', '')
 
-    telegramChats = driver.find_elements_by_xpath(
-        "//div[@class='message']")
+        print(phoneNumber)
 
-    chats = []
-    for currentChat in telegramChats:
-        chats.append(currentChat.text)
+        driver.execute_script(
+            "window.open('https://my.telegram.org','_blank');")
 
-    telegramCode = chats[len(chats) - 1]
-    telegramCode = telegramCode.split('\n')[1]
+        driver.switch_to.window(driver.window_handles[1])
 
-    print(telegramCode)
+        time.sleep(5)
 
-    driver.switch_to.window(driver.window_handles[1])
+        myPhoneNumber = driver.find_element_by_id("my_login_phone")
+        myBtnSubmit = driver.find_element_by_xpath("//button[@type='submit']")
 
-    time.sleep(5)
+        myPhoneNumber.send_keys(phoneNumber)
+        myBtnSubmit.click()
 
-    myPassword = driver.find_element_by_id("my_password")
+        driver.switch_to.window(driver.window_handles[0])
 
-    myPassword.send_keys(telegramCode)
-    myPassword.submit()
+        backBtn = driver.find_element_by_css_selector(
+            "button.btn-icon.tgico-left.sidebar-close-button")
+        backBtn.click()
 
-    time.sleep(5)
+        telegramMsg = driver.find_element_by_xpath(
+            "//li[@data-peer-id='777000']")
+        telegramMsg.click()
 
-    linkAPIdev = driver.find_element_by_xpath("//a[@href='/apps']")
-    linkAPIdev.click()
+        time.sleep(5)
 
-    time.sleep(5)
+        telegramChats = driver.find_elements_by_xpath(
+            "//div[@class='message']")
 
-    appInput = driver.find_elements_by_xpath(
-        "//span[@class='form-control input-xlarge uneditable-input'][@onclick='this.select();']")
+        chats = []
+        for currentChat in telegramChats:
+            chats.append(currentChat.text)
 
-    appData = []
-    for currentApp in appInput:
-        appData.append(currentApp.text)
+        telegramCode = chats[len(chats) - 1]
+        telegramCode = telegramCode.split('\n')[1]
 
-    if len(appData) == 0:
-        appTitleInput = driver.find_element_by_id("app_title")
-        appShortnameInput = driver.find_element_by_id("app_shortname")
-        platformCheckbox = driver.find_element_by_xpath(
-            "//input[@value='other']")
-        saveBtn = driver.find_element_by_id("app_save_btn")
+        print(telegramCode)
 
-        appTitleInput.send_keys("Barbara Muda")
-        appShortnameInput.send_keys("barbarmud123")
-        platformCheckbox.click()
+        driver.switch_to.window(driver.window_handles[1])
 
-        time.sleep(2)
+        time.sleep(5)
 
-        saveBtn.click()
+        myPassword = driver.find_element_by_id("my_password")
 
-        time.sleep(2)
+        myPassword.send_keys(telegramCode)
+        myPassword.submit()
+
+        time.sleep(5)
+
+        linkAPIdev = driver.find_element_by_xpath("//a[@href='/apps']")
+        linkAPIdev.click()
+
+        time.sleep(5)
 
         appInput = driver.find_elements_by_xpath(
             "//span[@class='form-control input-xlarge uneditable-input'][@onclick='this.select();']")
@@ -111,18 +106,54 @@ if (waitingDriver == True):
         for currentApp in appInput:
             appData.append(currentApp.text)
 
-        appId = appData[0]
-        appHash = appData[1]
-    else:
-        appId = appData[0]
-        appHash = appData[1]
+        if len(appData) == 0:
+            appTitleInput = driver.find_element_by_id("app_title")
+            appShortnameInput = driver.find_element_by_id("app_shortname")
+            platformCheckbox = driver.find_element_by_xpath(
+                "//input[@value='other']")
+            saveBtn = driver.find_element_by_id("app_save_btn")
 
-    with open('telebot.csv', 'w', encoding='UTF8', newline='') as CSVFile:
-        CSVWriter = csv.writer(CSVFile)
-        CSVWriter.writerow(['phoneNumber', 'appId', 'appHash'])
-        CSVWriter.writerow([phoneNumber, appId, appHash])
+            appTitleInput.send_keys("Barbara Muda")
+            appShortnameInput.send_keys("barbarmud123")
+            platformCheckbox.click()
 
-    subprocess.Popen("cp -r runner " + phoneNumber + "/")
-    
+            time.sleep(2)
 
-    driver.close()
+            saveBtn.click()
+
+            time.sleep(2)
+
+            appInput = driver.find_elements_by_xpath(
+                "//span[@class='form-control input-xlarge uneditable-input'][@onclick='this.select();']")
+
+            appData = []
+            for currentApp in appInput:
+                appData.append(currentApp.text)
+
+            appId = appData[0]
+            appHash = appData[1]
+        else:
+            appId = appData[0]
+            appHash = appData[1]
+
+        with open('telebot.csv', 'w', encoding='UTF8', newline='') as CSVFile:
+            CSVWriter = csv.writer(CSVFile)
+            CSVWriter.writerow(['phoneNumber', 'appId', 'appHash'])
+            CSVWriter.writerow([phoneNumber, appId, appHash])
+
+        runnerMaster = "runner"
+        runnerStored = "stored/" + phoneNumber
+
+        copytree(runnerMaster, runnerStored)
+
+        sessionFolder = runnerStored + '/session/'
+
+        telegramInit(phoneNumber, appId, appHash, sessionFolder)
+
+        driver.close()
+
+# DRIVE SAFE
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(telegramInit("+6283102594694", 8378843,
+             "661c0553ba2d6426960dcf011ea23afc", "stored/+6283102594694/session"))
--- runner/init_session.py
+++ runner/init_session.py
@@ -4,24 +4,24 @@ from telethon import sync, TelegramClient, events
 import json
 
 with open('config.json', 'r') as f:
-	config = json.loads(f.read())
+    config = json.loads(f.read())
 
 logging.basicConfig(level=logging.WARNING)
 
-accounts = config['accounts']
+account = config['account']
 
 folder_session = 'session/'
 
-for account in accounts:
-	api_id = account['api_id']
-	api_hash = account['api_hash']
-	phone = account['phone']
-	print(phone)
+api_id = account['api_id']
+api_hash = account['api_hash']
+phone = account['phone']
+print(phone)
 
-	client = TelegramClient(folder_session + phone, api_id, api_hash)
-	client.start()
-	if client.is_user_authorized():
-		print('Login success')
-	else:
-		print('Login fail')
-	client.disconnect()
+client = TelegramClient(folder_session + phone, api_id, api_hash)
+client.start()
+
+if client.is_user_authorized():
+    print('Login success')
+else:
+    print('Login fail')
+client.disconnect()
